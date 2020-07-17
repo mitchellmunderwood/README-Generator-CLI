8610ee1c713d76ad52bad8073fff2cccee32d166
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-07-17 19:11:50 -0400

    basic plan set up

---
+++ index.js
@@ -0,0 +1,9 @@
+// Build and Commit Plan
+
+// Get the skeleton set up
+// Get Input Questionare set up
+// get configuration object set up(have the top - down flow of the object mirror the eventual document)
+// get Readme hard file set up
+// Get progressive top down build, based on object
+// Record the video
+// create a github repo that hosts the video 
