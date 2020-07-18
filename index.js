42a701afb1c17b4f637201ae6b2f19f31de3cbdc
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-07-17 21:17:28 -0400

    Basic configuration questionaire flow is established.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Build and Commit Plan
 
-// Get the skeleton set up
-// Get Input Questionare set up
-// get configuration object set up(have the top - down flow of the object mirror the eventual document)
+// (done) Get the skeleton set up
+// (done) Get Input Questionare set up
+// (done) get configuration object set up(have the top - down flow of the object mirror the eventual document)
 // get Readme hard file set up
 // Get progressive top down build, based on object
 // Record the video
@@ -12,8 +12,57 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
 
-inquirer.prompt({
-
-}).then(function (data) {
+inquirer.prompt([
+    {
+        type: "input",
+        name: "user_github",
+        message: "Please input your github username."
+    },
+    {
+        type: "input",
+        name: "user_email",
+        message: "Please input your email address."
+    },
+    {
+        type: "input",
+        name: "project_title",
+        message: "Please input the project's title."
+    },
+    {
+        type: "input",
+        name: "project_description",
+        message: "Please input the project's description."
+    },
+    {
+        type: "input",
+        name: "project_installations",
+        message: "Please input the project's installation instructions."
+    },
+    {
+        type: "input",
+        name: "project_usage",
+        message: "Please input the project's usage information."
+    },
+    {
+        type: "input",
+        name: "project_contribution",
+        message: "Please input the project's contribution guidelines."
+    },
+    {
+        type: "input",
+        name: "project_test",
+        message: "Please input the project's test instructions."
+    },
+    {
+        type: "list",
+        name: "project_license",
+        choices: [
+            "license #1",
+            "license #2",
+            "license #3",
+        ]
+    },
+]).then(function (data) {
+    console.log(data);
 
 });
